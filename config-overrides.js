a042ffde43c3659935be9c680c49782c20eaf090
fritz-c <fritz-c@users.noreply.github.com>
2018-03-24 13:41:18 +0900

    chore: initial commit

---
+++ config-overrides.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies, no-param-reassign */
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireStyledComponents = require('react-app-rewire-styled-components');
+
+module.exports = function override(config, env) {
+  config = rewireReactHotLoader(config, env);
+  config = rewireStyledComponents(config, env);
+  return config;
+};
